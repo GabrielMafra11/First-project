e78722194b312463d6d56fc8c1f143d030d01b5a
Gabriel Mafra <gabrielmafraff@gmail.com>
2024-05-05 11:11:49 -0300

    adjust container widht

--- .vscode/script.js
+++ .vscode/script.js
@@ -1,21 +1,16 @@
 function toggleMode() {
   const html = document.documentElement
-  /*if (html.classList.contains("light")) {
-    html.classList.remove("light")
-  } else {
-    html.classList.add("light")
-  }  ((((funçao do item abaixo)))))*/
   html.classList.toggle("light")
 
-  // pegar a tag img(imagem)
+  // pegar a tag img
   const img = document.querySelector("#profile img")
 
-  //substituir a imagem
+  // substituir a imagem
   if (html.classList.contains("light")) {
     // se tiver light mode, adicionar a imagem light
     img.setAttribute("src", "./assets/avatar-light.png")
   } else {
-    //se tiver sem light mode,manter a imagem normal
-    img.setAttribute("src", "./assets/avatar,png")
+    // set tiver sem light mode, manter a imagem normal
+    img.setAttribute("src", "./assets/avatar.png")
   }
 }
